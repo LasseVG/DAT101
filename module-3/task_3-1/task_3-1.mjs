ccd168ba1dccef3952e2c2778a8319c9466ac34d
Lasse Viking Gudding <lassevg@uia.no>
2026-09-18 18:39:42 +0200

    3.1 alt ferdig

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -2,7 +2,7 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-const wakeUpTime = 7;
+const wakeUpTime = 8;
 const time = 7;
 if(wakeUpTime === time) {
     printOut("Take the bus!");
@@ -46,7 +46,7 @@ printOut(newLine);
 ("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 
-printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
+printOut("--- Part 8 og 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 let arrayMonth = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
 const noOfMonth = arrayMonth.length;
@@ -58,12 +58,48 @@ printOut("The month is " + monthName + " and you do not need to take Vitamin D")
 }
 printOut(newLine);
 
-printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+switch(monthName){
+     case "january":31
+     case "march":31
+     case "may":31
+     case "july":31
+     case "august":31
+     case "october":31 
+     case "december":31
+     printOut ("31 Days in a month.")
+     break;
+     case "february":28
+printOut ("28 Days in a month.")
+    break;
+    default:
+printOut ("30 Days in a month.")
+
+}
+
 
 printOut(newLine);
+
+
+("--- Part 9 ----------------------------------------------------------------------------------------------");
+/* Put your code below here!*/
+
 
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-
-printOut(newLine);
+/*let ArrayMonth = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+const NoOfMonth = ArrayMonth.length;
+const MonthName = ArrayMonth[Math.floor(Math.random() * NoOfMonth)];
+if (MonthName === "march" || MonthName === "may" || MonthName === "april") {
+    printOut("The month is " + MonthName + " The Museum is closed");
+} else if (MonthName === "april") {
+    printOut("Sorry, the month is april and the main gallery is closed, you are welcome into the premises next door");
+} else {
+    printOut("The month is " + MonthName + " and the museum is open");
+}*/
+if (monthName === "March" || (monthName === "May")){
+    printOut ("sorry, the gallery is closed")
+}else if(monthName === "april"){
+printOut ("sorry, the gallery is closed, but you are welcome into the premise next door");
+}else{
+    printOut("welcome to my gallery");
+}
