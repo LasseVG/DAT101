52d0f9f9292880c64ba503452bff23a83fa79429
Lasse Viking Gudding <lassevg@uia.no>
2026-09-18 14:28:54 +0200

    3.1 part 1 2 3

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -2,8 +2,15 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-
+const wakeUpTime = 7;
+const time = 7;
+if(wakeUpTime === time) {
+    printOut("Take the bus!");
+} else if (time === 8) { 
+    printOut ("Take the train");
+}else{
+    printOut("Take the car");
+}
 printOut(newLine);
 
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
@@ -23,7 +30,14 @@ printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-
+let arrayMonth = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+const noOfMonth = arrayMonth.length;
+const monthName = arrayMonth[Math.floor(Math.random() * noOfMonth)];
+if (monthName.includes("r")) {
+printOut("The month is " + monthName + " and you need to take Vitamin D");
+} else {
+printOut("The month is " + monthName + " and you do not need to take Vitamin D");
+}
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
